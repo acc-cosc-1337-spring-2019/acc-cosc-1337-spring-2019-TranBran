617fccde0f95c828d59fb7f93b4c7009fb7091f9
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-01-30 18:26:44 -0600

    c++ is case sensitive

--- classwork/02_assign/assign02.cpp
+++ classwork/02_assign/assign02.cpp
@@ -1,7 +1,7 @@
 //Write the function code that returns the product of hours and hourly_rate.
 #include "assign02.h"
 
-double Gross_Pay(double hours, double hourly_rates)
+double Gross_pay(double hours, double hourly_rates)
 {
 	return hours * hourly_rates;
 
