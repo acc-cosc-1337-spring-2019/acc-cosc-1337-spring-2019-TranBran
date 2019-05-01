b813b60a743d10a11855287017dda7185678aa9c
8385214 <brandon.tran1@g.austincc.edu>
2019-04-30 19:04:14 -0500

    module 11 in class

--- examples/11_module/06_destructors/destructor.h
+++ examples/11_module/06_destructors/destructor.h
@@ -1,1 +1,18 @@
 //
+class Destructor
+{
+public:
+	Destructor(int n) : num(n) { ptr_num = new int(n); }
+	Destructor(const Destructor& d);
+	Destructor& operator=(Destructor& d); // copy assignment - rule of 3
+	~Destructor(); // Rule of 3
+	void set_value(int n);
+	int get_value();
+	
+	void set_ptr_num(int n);
+	int* get_ptr_num();
+private:
+	int num;
+	
+	int* ptr_num;
+};
--- examples/11_module_test/11_module_test.cpp
+++ examples/11_module_test/11_module_test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "ref_pointers.h"
-
+#include "destructor.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
@@ -14,6 +14,40 @@ TEST_CASE("Test function reference parameter")
 	ref(number,numberptr);
 	REQUIRE(number == 100);
 
+
+
+}
+
+TEST_CASE("Test class copy")
+{
+
+	Destructor a(10);
+	Destructor b = a;
+
+	REQUIRE(a.get_value() == 10);
+	REQUIRE(b.get_value() == 10);
+
+	a.set_value(5);
+
+	REQUIRE(a.get_value() == 5);
+	REQUIRE(b.get_value() == 10);
+}
+
+TEST_CASE("Test class copy Dynamic Private Data")
+{
+
+	Destructor a(10);
+	Destructor b = a;
+	int* ptr = a.get_ptr_num();
+	int* ptr1 = b.get_ptr_num();
+
+	REQUIRE(*ptr == 10);
+	REQUIRE(*ptr1 == 10);
+
+	a.set_ptr_num(100);
+
+	REQUIRE(*ptr == 100);
+	REQUIRE(*ptr1 == 10);
 
 
 }
